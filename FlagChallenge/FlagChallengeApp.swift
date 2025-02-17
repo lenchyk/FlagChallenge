b4d1ca5a6afebf126ca640536d8a2ba621fbbb43
Lena Soroka <4ta.lenasoroka.1989@gmail.com>
2025-02-17 18:48:48 +0200

    Initial Commit

---
+++ FlagChallenge/FlagChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagChallengeApp.swift
+//  FlagChallenge
+//
+//  Created by Lena Soroka on 17.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlagChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
